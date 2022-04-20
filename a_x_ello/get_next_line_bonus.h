fdac2337e07e2ffdf94a3b407432e4d3ba5acad8
Nacho Donaire <ndonaire@c1r3s6.42madrid.com>
2022-04-20 13:41:35 +0200

    got it

--- a_x_ello/get_next_line_bonus.h
+++ a_x_ello/get_next_line_bonus.h
@@ -6,12 +6,12 @@
 /*   By: ndonaire <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 12:40:09 by ndonaire          #+#    #+#             */
-/*   Updated: 2022/04/20 13:37:05 by ndonaire         ###   ########.fr       */
+/*   Updated: 2022/04/20 13:37:00 by ndonaire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # include <unistd.h>
 # include <sys/types.h>
 # include <stdio.h>
